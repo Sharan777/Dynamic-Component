fd1165ca313798e626eaae532a825d5747d4378a
Sharan777 <sharangouda021@gmail.com>
2019-05-30 10:50:55 +0530

    8 files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PopupComponent } from './popup/popup.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { PopupService } from './popup/popup.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, PopupComponent, DeveloperComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [PopupService]
 })
 export class AppModule { }
