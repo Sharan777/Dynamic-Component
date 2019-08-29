140a0f0a416f2d5796ce5f526a962dd7af6f4aff
Sharan777 <sharangouda021@gmail.com>
2019-08-29 17:15:21 +0530

    2 files modified

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, ViewChild,
-ComponentFactoryResolver } from '@angular/core';
+import {
+  Component, OnInit, ViewChild,
+  ComponentFactoryResolver
+} from '@angular/core';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
-export class PopupComponent implements OnInit { 
+export class PopupComponent implements OnInit {
 
   constructor() { }
   ngOnInit() { }
