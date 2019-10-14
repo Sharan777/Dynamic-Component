7d17569ed0226af9e0cff31b1b0c738253429eac
Sharan777 <sharangouda021@gmail.com>
2019-10-14 16:28:26 +0530

    1 file modified

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -13,12 +13,11 @@ export class PopupComponent implements OnInit {
   constructor() { }
   ngOnInit() { }
 
-  deletComp() {
+    deletComp() {
     let element = document.getElementsByTagName('app-popup'), index;
     console.log(element);
-    debugger;
     for (index = element.length - 1; index >= 0; index--) {
-      element[index].parentNode.removeChild(element[index]);
+    element[index].parentNode.removeChild(element[index]);
     }
   }
 
